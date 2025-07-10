440f7951a5146274ac2bff0608099c6f3a3ef53f
Melissa Aoko <melissa.aoko@ucalgary.ca>
2025-07-09 19:50:39 -0600

    added some logic

--- grid.py
+++ grid.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 class Grid:
     def __init__(self):
         self.num_rows = 20
@@ -7,8 +10,30 @@ class Grid:
         #2d array to represet grids , list of lists
         self.grid =[[0 for j in range(self.num_cols)]for i in range(self.num_rows)]
 
+        self.colors = self.get_cell_colors()
+
     def print_grid(self):
         for row in range (self.num_rows):
             for column in range (self.num_cols):
                 print(self.grid[row][column], end=" ")
             print()
+
+
+    def get_cell_colors(self):
+        dark_grey   = (26, 31, 40)
+        cyan        = (163, 216, 244) 
+        yellow      = (255, 246, 165)  
+        purple      = (217, 184, 255)  
+        mint_green  = (182, 245, 216)  
+        coral_pink  = (255, 181, 181)  
+        peach       = (255, 209, 164)  
+        rose        = (255, 183, 227)  
+
+        return [dark_grey,cyan, yellow, purple, mint_green, coral_pink, peach, rose]
+
+    def draw (self, screen):
+        for row in range(self.num_rows):
+            for column in range (self.num_cols):
+                cell_value = self.grid[row][column]
+                cell_rect= pygame.Rect(column*self.cell_size +1, row*self.cell_size+1, self.cell_size -1, self.cell_size-1)
+                pygame.draw.rect(screen, self.colors[cell_value],cell_rect)
--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-import pygame
+import pygame, sys
+from grid import Grid
 
 pygame.init()
-dark_blue=(44,44,127)
+blush_pink= (255, 233, 236)
 
 screen = pygame.display.set_mode((300,600))
 pygame.display.set_caption("Python Tetris")
 
 clock= pygame.time.Clock()
 
+game_grid=Grid()
+
+game_grid.grid[0][0] = 1
+game_grid.grid[3][5] = 4
+game_grid.grid[17][8] = 7
+
+game_grid.print_grid()
+
 #game loop
 while True:
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
             pygame.quit()
-            SystemExit() #sys.exit()
+            sys.exit() 
     #drawing
 
-    screen.fill(dark_blue)
+    screen.fill(blush_pink)
+    game_grid.draw(screen)
+
+
     pygame.display.update()
     clock.tick(60)
